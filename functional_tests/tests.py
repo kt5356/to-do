f10be8f0e971a3ae527ee0d618c9f764b1978ff9
Kevin Tahmoresi <kevin.tahmoresi@workday.com>
2018-08-28 07:32:17 -0700

    new URL + view for adding to existing lists. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -62,7 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
+        self.assertRegex(edith_list_url, '/lists/.+/')
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
@@ -73,12 +73,12 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy Milk')
+        inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy Milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertRegex(francis_list_url, '/lists/.+/')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
