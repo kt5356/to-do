667501fdc3ee53c5382cb23a2bec6490230aafb5
Kevin Tahmoresi <kevin.tahmoresi@workday.com>
2018-08-31 09:12:15 -0700

    test build

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
+import os 
+import time 
+import unittest
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import time 
-import unittest 
+
 
 MAX_WAIT = 10
 
@@ -11,6 +14,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
